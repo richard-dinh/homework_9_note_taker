e7113954f6e3d9d27bfdbb6cf32ca50c8ec9e409
pinheadrarry <richarddinh@gmail.com>
2020-02-20 14:50:40 -0800

    wrote .get for /api/notes

--- server.js
+++ server.js
@@ -7,23 +7,22 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-//returns all the notes in db.json
-// app.get('/notes', (request, response) => {
-//   fs.readFile(path.join(__dirname, 'db', 'db.json'),'utf8', (error, data) => {
-//     if(error){
-//       console.error(error)
-//     }
-//     let notes = JSON.parse(data)
-//     console.log(notes)
-//     response.json(notes)
-//   })
-// })
-
 app.get('/notes', (request, response) => {
   //must provide absolute path to sendFile even if you have static
   response.sendFile(path.join(__dirname, 'public', 'notes.html'))
 })
 
+//returns all the notes in db.json
+app.get('/api/notes', (request, response) => {
+  fs.readFile(path.join(__dirname, 'db', 'db.json'),'utf8', (error, data) => {
+    if(error){
+      console.error(error)
+    }
+    let notes = JSON.parse(data)
+    console.log(notes)
+    response.json(notes)
+  })
+})
 
 //passes in the port given by heroku or 3000
 app.listen(process.env.PORT || 3000)
